0e8120365c9cc598d87ff53d146a98c86de490cf
woong-sung <cjswo138@gmail.com>
2024-05-05 12:00:21 +0900

    등록주식 불러오기 줄바꿈

--- getStocks.php
+++ getStocks.php
@@ -5,11 +5,11 @@ include __DIR__ . '/config.php';
 $query = "select * from stock";
 // $query = "show databases";
 $data = mysqli_query($conn, $query);
-$result = "등록된 주식은 다음과 같습니다.\n";
+$result = "등록된 주식은 다음과 같습니다.</br>";
 
 $i=1;
 while ($row = mysqli_fetch_assoc($data)) {
-  $result.=$i.") 종목 이름: ".$row['name'].", "."종목 코드: ".$row['code'];
+  $result.=$i.") 종목 이름: ".$row['name'].", "."종목 코드: ".$row['code']."</br>";
   $i++;
 }
 
